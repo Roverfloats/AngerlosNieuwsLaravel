e0b8e7c65843dd5f69613a06884f84099d05d9a8
Roverfloats <tygoliebers@gmail.com>
2024-05-30 16:12:52 +0200

    reworked archive a bit

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -15,26 +15,72 @@
 
     <div class="container content-archive">
         <div class="wrap">
+            <div class="col item-archive">
+                <div class="news-paper-archive">
 
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2069
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
+            <div class="col item-archive">
+                <div class="news-paper-archive">
+
+                </div>
+                <h3 class="center-text">
+                    maart 2050
+                </h3>
+            </div>
         </div>
     </div>
-
-    {{-- <script>
-        function updateColumnGap() {
-            const container = document.querySelector('.wrap');
-            const containerWidth = container.clientWidth;
-            const itemWidth = 315;
-            const minGap = 20;
-            const itemsPerRow = Math.floor(containerWidth / (itemWidth + minGap));
-            const totalItemsWidth = itemsPerRow * itemWidth;
-            const remainingSpace = containerWidth - totalItemsWidth;
-            const newGap = remainingSpace / (itemsPerRow - 1);
-            container.style.columnGap = `${Math.max(newGap, minGap)}px`;
-        }
-
-        window.addEventListener('resize', updateColumnGap);
-        document.addEventListener('DOMContentLoaded', updateColumnGap);
-    </script> --}}
 </body>
 
 </html>
