f5650f6aef015b5337922e586bb1e7b5a85a8162
Karsten <karstenseijkens@gmail.com>
2024-06-18 10:51:39 +0200

    removed more death code

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VariableConverter;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,13 +17,3 @@ Route::get('/article', function() {
 Route::get('/newspaper', function () {
     return view('news-paper');
 });
-// Route::post('/process-variable', function (Illuminate\Http\Request $request) {
-//     $receivedVariable = $request->input('variable');
-    
-//     // Now you can use $receivedVariable in your PHP code
-    
-//     // For example, you can echo it:
-//     return "Received variable in PHP: " . $receivedVariable;
-// });
-
-// Route::post('/convert-variable', [VariableConverter::class, 'convertVariable']);
