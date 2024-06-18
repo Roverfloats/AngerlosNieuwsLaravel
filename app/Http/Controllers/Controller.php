2aa944dc55328b060750be2036981d16987171ab
Karsten <karstenseijkens@gmail.com>
2024-06-18 10:54:00 +0200

    removed hpofully last piece of death page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,21 +5,5 @@ use Illuminate\Http\Request;
 
 abstract class Controller
 {
-    public function yourMethod()
-    {
 
-    }
-
-    public function convertVariable(Request $request)
-    {
-        $javascriptVariable = $request->input('javascript_variable');
-
-        // Do any processing or manipulation of the JavaScript variable here
-        // For example, you could convert it to uppercase
-        $phpVariable = strtoupper($javascriptVariable);
-    
-        // Return a response containing the PHP variable back to the client-side JavaScript
-        return response()->json(['php_variable' => $phpVariable]);
-
-    }
 }
