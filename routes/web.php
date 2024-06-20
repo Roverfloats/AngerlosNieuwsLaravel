69bf7c4417bc8cc7542fbe17abdc0c4844824b49
Khalil <khalilhenane@hotmail.com>
2024-06-20 12:00:50 +0200

    Article upload function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+// use App\Http\Controllers\article;
+use App\Http\Controllers\articles;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,17 @@ Route::get('/', function () {
 Route::get('/archive.html', function () {
     return view('archive');
 });
+
+
+Route::post('/createArticle', [articles::class, 'createArticle'])->name('createArticle');
+
+// TODO  replace this with the actual page of the article display
+Route::get('/article-editor', [articles::class, 'displayInformation'])->name('displayInformation');
+
+
+// TODO  Delete this route, before commiting 
+Route::get('/', function () {
+    return view('upload-article');
+});
+
+Route::get('{id}', [articles::class, 'deleteArticle'])->name('deleteArticle');
